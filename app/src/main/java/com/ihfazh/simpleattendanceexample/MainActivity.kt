0920738d9c222a4a3661504cfc638538711acac8
ihfazhillah <mihfazhillah@gmail.com>
2021-06-16 22:18:18 +0700

    add event listeners to each buttons

--- app/src/main/java/com/ihfazh/simpleattendanceexample/MainActivity.kt
+++ app/src/main/java/com/ihfazh/simpleattendanceexample/MainActivity.kt
@@ -2,10 +2,26 @@ package com.ihfazh.simpleattendanceexample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.ihfazh.simpleattendanceexample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.buttonMore.setOnClickListener {
+            Toast.makeText(this, "Button More Clicked", Toast.LENGTH_SHORT).show()
+        }
+        binding.eventsCard.setOnClickListener {
+            Toast.makeText(this, "Events Card Clicked", Toast.LENGTH_SHORT).show()
+        }
+        binding.studentsCard.setOnClickListener {
+            Toast.makeText(this, "Students Card Clicked", Toast.LENGTH_SHORT).show()
+        }
     }
 }
