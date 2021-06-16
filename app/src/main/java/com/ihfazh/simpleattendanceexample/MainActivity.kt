70aa8c9412417862f57f87ffdc765360bb43c943
ihfazhillah <mihfazhillah@gmail.com>
2021-06-16 22:32:54 +0700

    implement menu and set on click listener

--- app/src/main/java/com/ihfazh/simpleattendanceexample/MainActivity.kt
+++ app/src/main/java/com/ihfazh/simpleattendanceexample/MainActivity.kt
@@ -2,6 +2,8 @@ package com.ihfazh.simpleattendanceexample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.PopupMenu
 import android.widget.Toast
 import com.ihfazh.simpleattendanceexample.databinding.ActivityMainBinding
 
@@ -15,13 +17,32 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.buttonMore.setOnClickListener {
-            Toast.makeText(this, "Button More Clicked", Toast.LENGTH_SHORT).show()
+            openMoreMenu(it)
         }
         binding.eventsCard.setOnClickListener {
             Toast.makeText(this, "Events Card Clicked", Toast.LENGTH_SHORT).show()
         }
         binding.studentsCard.setOnClickListener {
             Toast.makeText(this, "Students Card Clicked", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun openMoreMenu(view: View) {
+        PopupMenu(this, view).apply {
+            inflate(R.menu.more_button)
+            setOnMenuItemClickListener { menuItem ->
+                when(menuItem.itemId){
+                    R.id.menu_event -> {
+                        Toast.makeText(this@MainActivity, "Create event menu clicked", Toast.LENGTH_SHORT).show()
+                    }
+                    R.id.menu_student -> {
+                        Toast.makeText(this@MainActivity, "Create student menu clicked", Toast.LENGTH_SHORT).show()
+                    }
+                }
+
+                true
+            }
+            show()
         }
     }
 }
