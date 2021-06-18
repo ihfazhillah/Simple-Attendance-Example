d4926a89a1b2456452b5f3a064e1184ae581cd50
ihfazhillah <mihfazhillah@gmail.com>
2021-06-18 21:55:08 +0700

    up and title respect to label in the nav configurations

--- app/src/main/java/com/ihfazh/simpleattendanceexample/MainActivity.kt
+++ app/src/main/java/com/ihfazh/simpleattendanceexample/MainActivity.kt
@@ -5,16 +5,29 @@ import android.os.Bundle
 import android.view.View
 import android.widget.PopupMenu
 import android.widget.Toast
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.ihfazh.simpleattendanceexample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var navHostController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val navFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navHostController = navFragment.findNavController()
+        setupActionBarWithNavController(navHostController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navHostController.navigateUp()
     }
 
 }
