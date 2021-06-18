8a59f16e49a7982e93bdb1d21e6fca17ae71e805
ihfazhillah <mihfazhillah@gmail.com>
2021-06-18 21:46:19 +0700

    add navigate up

--- app/src/main/java/com/ihfazh/simpleattendanceexample/ui/studentform/StudentFormFragment.kt
+++ app/src/main/java/com/ihfazh/simpleattendanceexample/ui/studentform/StudentFormFragment.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.ihfazh.simpleattendanceexample.databinding.FragmentStudentFormBinding
 
 class StudentFormFragment: Fragment() {
@@ -19,6 +21,14 @@ class StudentFormFragment: Fragment() {
     ): View {
         _binding = FragmentStudentFormBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.btnSave.setOnClickListener {
+            Toast.makeText(context, "Student will be saved into database.", Toast.LENGTH_SHORT).show()
+            findNavController().navigateUp()
+        }
     }
 
     override fun onDestroyView() {
